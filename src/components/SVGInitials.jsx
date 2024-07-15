bbfd6b50f6a8aea17d6ad32a6b35dceabf015cfe
GalaxyBuilder-oss <galaxybuilder.oss@gmail.com>
2024-07-16 06:15:59 +0700

    feat: add SVG as image profile instead span with style

---
+++ src/components/SVGInitials.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/prop-types */
+const SVGInitials = ({ words, backgroundColor, textColor }) => {
+
+  return (
+    <>
+      <svg width="56" height="56" xmlns="http://www.w3.org/2000/svg" className={backgroundColor + " rounded-full"}>
+        <rect width="100%" height="100%" />
+        <text x="50%" y="50%" dominantBaseline="middle" textAnchor="middle" fontSize="18" fontWeight={"bold"} className={textColor}>
+          {words}
+        </text>
+      </svg>
+    </>
+  );
+};
+
+export default SVGInitials
